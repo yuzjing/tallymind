b56d4e6fc87cf08ac3282bfc182b6e52b701e8cd
yuzjing <yuzhao-git@outlook.com>
2026-08-12 13:07:56 +0800

    finish common api logic

--- internal/handler/transaction.go
+++ internal/handler/transaction.go
@@ -1,4 +1,4 @@
-// internal/handler/handler.go
+// internal/handler/transaction.go
 
 package handler
 
@@ -31,8 +31,8 @@ func NewTransactionHandler(ledgerCfg config.LedgerConfig) *TransactionHandler {
 func (h *TransactionHandler) handleTransaction(c *gin.Context) {
 	// 获取请求header
 	userID := cmp.Or(c.GetHeader("X-User-ID"), h.cfg.DefaultReporter)
-	sourceChannel := cmp.0r(c.GetHeader("X-Source-Channel"), "unknown_api_plugin")
-	
+	sourceChannel := cmp.Or(c.GetHeader("X-Source-Channel"), "unknown_api_plugin")
+
 	// 组装请求体传输层上下文
 	reqCtx := ledger.RequestContext{
 		UserID:           userID,
@@ -42,7 +42,7 @@ func (h *TransactionHandler) handleTransaction(c *gin.Context) {
 		FallbackCategory: h.cfg.FallbackCategory,
 		FallbackAccount:  h.cfg.FallbackAccount,
 	}
-	// 处理请求
+	// Gin 的极简 JSON 绑定与自动校验
 	var req ledger.BatchTransactions
 	if err := c.ShouldBindJSON(&req); err != nil {
 		log.Printf("[WARN] API 请求 JSON 解析失败: %v\n", err)
@@ -52,7 +52,17 @@ func (h *TransactionHandler) handleTransaction(c *gin.Context) {
 		})
 		return
 	}
-	log.Printf("[INFO] 成功记账 %d 笔| 发送者：%s\n", len(req.Transactions), userID)
+	// 调用 ledger 处理函数
+	if err := ledger.AppendBatchTransactions(h.cfg.FilePath, req, reqCtx); err != nil {
+		log.Printf("[ERROR] 账单写入失败: %v\n", err)
+		c.JSON(http.StatusBadRequest, APIResponse{
+			Code:    400,
+			Message: err.Error(),
+		})
+		return
+	}
+	// 返回成功响应
+	log.Printf("[INFO] 成功记账 %d 笔| 发送者：%s | 渠道: %s\n", len(req.Transactions), userID, sourceChannel)
 	c.JSON(http.StatusOK, APIResponse{
 		Code:    0,
 		Message: "已成功为你记录账单！",
